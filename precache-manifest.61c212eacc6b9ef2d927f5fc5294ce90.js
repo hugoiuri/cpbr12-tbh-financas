b53395325d2f04c21c87e1d7186058fd2f8123ac
hugoiuri <hugoraiuri@gmail.com>
2019-02-15 13:33:22 -0200

    Updates

---
+++ precache-manifest.61c212eacc6b9ef2d927f5fc5294ce90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff01bed8c650a7f90a06",
+    "url": "/cpbr12-tbh-financas/static/js/runtime~main.ff01bed8.js"
+  },
+  {
+    "revision": "d2be3eceb7eb39cb73ab",
+    "url": "/cpbr12-tbh-financas/static/js/main.d2be3ece.chunk.js"
+  },
+  {
+    "revision": "28ac56a44384f082c2b1",
+    "url": "/cpbr12-tbh-financas/static/js/1.28ac56a4.chunk.js"
+  },
+  {
+    "revision": "d2be3eceb7eb39cb73ab",
+    "url": "/cpbr12-tbh-financas/static/css/main.4acd03e4.chunk.css"
+  },
+  {
+    "revision": "28ac56a44384f082c2b1",
+    "url": "/cpbr12-tbh-financas/static/css/1.dffc23dd.chunk.css"
+  },
+  {
+    "revision": "8228be321585fc3a6ead41d174f144ff",
+    "url": "/cpbr12-tbh-financas/index.html"
+  }
+];
